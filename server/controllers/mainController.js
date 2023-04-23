57b8abbf370a5fde45144fe4bdefcbb6ae8fcf45
Sinister-00 <sswapnil_be20@thapar.edu>
2023-04-24 04:43:41 +0530

    Final Project
    Signed-off-by: Sinister-00 <sswapnil_be20@thapar.edu>

---
+++ server/controllers/mainController.js
@@ -0,0 +1,34 @@
+/**
+ * GET /
+ * Homepage 
+*/
+exports.homepage = async (req, res) => {
+  const locals = {
+    title: "NodeJs Notes",
+    description: "Free NodeJS Notes App.",
+  }
+  res.render('index', {
+    locals,
+    layout: '../views/layouts/front-page'
+  });
+}
+
+
+/**
+ * GET /
+ * About 
+*/
+exports.about = async (req, res) => {
+  const locals = {
+    title: "About - NodeJs Notes",
+    description: "Free NodeJS Notes App.",
+  }
+  res.render('about', locals);
+}
+exports.features = async (req, res) => {
+  const locals = {
+    title: "Features - NodeJs Notes",
+    description: "Free NodeJS Notes App.",
+  }
+  res.render('features', locals);
+}
